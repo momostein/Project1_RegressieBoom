1812049321894e60c9610f6a1ad80b9eab7b6104
Brecht Ooms <brecht.ooms@hotmail.com>
2019-04-26 12:59:30 +0200

    Added better comments to evaluation

--- Project1_RegressieBoom/evaluation.cpp
+++ Project1_RegressieBoom/evaluation.cpp
@@ -2,36 +2,38 @@
 
 #include "evaluation.h"
 
-// TODO: Better comments
-
 namespace eval
 {
 	bool Evaluation::evaluate(const organ::Organ & organ)
 	{
+		// should be changed to float in case a numeric property is added to organ
 		bool val;
 
+		// Check wich field should be evaluated
+		// and calculate the value
 		switch (field)
 		{
 		case eval::Evaluation::MODEL:
-			val = model == organ.model;
+			val = (model == organ.model);
 			break;
 		case eval::Evaluation::CONDITION:
-			val = condition == organ.condition;
+			val = (condition == organ.condition);
 			break;
 		case eval::Evaluation::LESLIE:
-			val = leslie == organ.leslie;
+			val = (leslie == organ.leslie);
 			break;
 		default:
 			val = false;
 			break;
 		}
-		
+
+		// Return the result of the selected comparison
 		switch (comp)
 		{
 		case '>':
-			return val > comp_val;
+			return (val > comp_val);
 		case '<':
-			return val < comp_val;
+			return (val < comp_val);
 		default:
 			return false;
 		}
@@ -49,13 +51,17 @@ namespace eval
 		// Search the string for an evaluation format
 		if (!std::regex_search(eval_string, m, e))
 		{
+			//If no matches were found, throw an exception
 			std::string error = "Wrong name format for an evaluation: " + eval_string;
 			throw std::exception(error.c_str());
 		}
-			
 
-		std::string sField = m[1];
-		std::string sFieldVal = m[2];
+
+		// Get the strings from the capture groups
+		std::string sField = m[1]; // The Field
+		std::string sFieldVal = m[2]; // The Value of the field
+
+		// Save the right settings for the right fields
 		if (sField == "Model")
 		{
 			field = MODEL;
@@ -64,39 +70,44 @@ namespace eval
 		else if (sField == "Condition")
 		{
 			field = CONDITION;
+
+			// Convert the string to the enum for performance
 			if (sFieldVal == "fair")
 				condition = organ::Condition::FAIR;
 			else if (sFieldVal == "good")
 				condition = organ::Condition::GOOD;
 			else if (sFieldVal == "excellent")
 				condition = organ::Condition::EXCELLENT;
-			
-			else 
+			else
 			{
+				// If no matching condition is found, throw an exception
 				std::string error = "Unknow Condition: " + sFieldVal;
 				throw std::exception(error.c_str());
 			}
-				
+
 		}
 		else if (sField == "Leslie")
 		{
 			field = LESLIE;
+
+			// LESLIE is a boolean, and can be "yes" or something else:
+			// "yes"			==> true
+			// anything else	==> false
 			leslie = sFieldVal == "yes";
-			
+
 		}
 		else
 		{
+			// If no matching field is found, throw an exception
 			std::string error = "Unknow Field: " + sField;
 			throw std::exception(error.c_str());
 		}
 
-		comp = m[3].str()[0]; //Grab first char from the string
+		// Get the comparison operator char
+		// It's the first and only char of the operator capture group
+		comp = m[3].str()[0];
 
+		// Convert the captured value string to a float
 		comp_val = std::stof(m[4]);
-	}
-
-	Evaluation::Evaluation()
-	{
-
 	}
 }
--- Project1_RegressieBoom/evaluation.h
+++ Project1_RegressieBoom/evaluation.h
@@ -3,34 +3,41 @@
 
 #include "organ.h"
 
-// TODO: Better comments
-
 namespace eval
 {
+	// A class that store the decision for one node in the regression tree
 	class Evaluation
 	{
 	public:
+		// The possible property fields
 		enum Field {
 			MODEL,
 			CONDITION,
 			LESLIE,
 		};
 
+		// Evaluate the organ
 		bool evaluate(const organ::Organ& organ);
 
+		// Constructor from a "name" inside a node
 		Evaluation(const std::string& eval_string);
-		Evaluation();
+
+		// Default constructor
+		Evaluation() {};
 
 	private:
+		// Wich property field the evaluation should look at
 		Field field;
 
-		// Field value
+		// Field values
 		std::string model;
 		organ::Condition condition;
 		bool leslie;
 
+		// The comparison operator that should be used
 		char comp;
 
+		// The value that the property value should be compared to
 		float comp_val;
 
 	};
